6ffebe68c244c92de9192657360ad8bc123453eb
ADMIN <ADMIN@ADMIN-PC>
2019-09-20 23:48:30 +0700

    Latihan-Java

---
+++ src/pbo11/pkg10118910/latihan15/PBO1110118910Latihan15.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11.pkg10118910.latihan15;
+
+/**
+ *
+ * @author 
+ * NAMA  : Muhamad Syifa Amrulloh 
+ * KELAS : PBO11K 
+ * NIM   : 10118910
+ *
+ * Description Program : Program ini Berisi pengunaan Logika
+ */
+public class PBO1110118910Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
